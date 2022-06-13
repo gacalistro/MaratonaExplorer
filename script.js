4cbd48eba9f7a0a27157397ff264dad06cfe36b9
Gabriel Calistro <gahcalistro@gmail.com>
2022-06-13 19:56:49 -0300

    First commit: function toggle button

---
+++ script.js
@@ -0,0 +1,36 @@
+const possibleAnswers = ["s", "n", "n tbm", "nunca", "?", "quem?"];
+
+const inputElement = document.querySelector("input");
+
+const btnElement = document.querySelector("button");
+
+// DISABLE BUTTON ON LOAD
+window.onload = () => changingButtonState();
+
+// CHECK IF INPUT VALUE TO TOGGLE BUTTON STATE
+function changingButtonState() {
+  if (inputElement.value.length == 0) {
+    btnElement.setAttribute("disabled", true);
+  } else {
+    btnElement.removeAttribute("disabled");
+  }
+}
+
+// GENERATE ANSWER
+function answer() {
+  const randomNumber = Math.floor(Math.random() * possibleAnswers.length);
+
+  const asking = `<div>${inputElement.value}</div>`;
+
+  const answer = document.querySelector("#answer");
+  answer.innerHTML = asking + possibleAnswers[randomNumber];
+
+  inputElement.value = "";
+  changingButtonState();
+
+  answer.style.opacity = 1;
+
+  setTimeout(() => {
+    answer.style.opacity = 0;
+  }, 3000);
+}
